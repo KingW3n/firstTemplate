db2351c285180fd5b5d30ee18a95d179f4b1d42a
KingW3n <wendeljr01@gmail.com>
2023-10-17 19:41:44 -0300

    incluindo botao tema

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3b1c8ebceff35c38"; }
+/******/ 		__webpack_require__.h = function() { return "eda39ecd8056edad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
